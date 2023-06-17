0b1d6bd694d8e69bdeb6b76434e5c9986ad98780
noli-noli <gp20a051@oecu.jp>
2023-06-18 06:23:34 +0900

    利用できないスクリプトの削除

--- models/pre_download.sh
+++
@@ -1,9 +0,0 @@
-#シェル内で本ファイルを実行
-
-
-#Googleドライブで共有したURL内に存在するIDを定義
-ID="1c5v7q8syD_9r3IDJM3_g7Un8RnoM1Mdm"
-#保存するファイル名
-FILE="pretrained_model.bin"
-
-wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://drive.google.com/uc?export=download&id=$ID" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$ID" -O $FILE && rm -rf /tmp/cookies.txt
